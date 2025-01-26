17596f118ea4c400cbf0c4d1e5efe67d6e6c77b6
Marcelis Dun <149423823+duma12322@users.noreply.github.com>
2025-01-25 21:33:15 -0400

    Update agendar_cita_adolescente_presencial2.php

--- app/vistas/agendar_cita_adolescente_presencial2.php
+++ app/vistas/agendar_cita_adolescente_presencial2.php
@@ -201,7 +201,7 @@ $result_agenda = $stmt_agenda->fetchAll(PDO::FETCH_ASSOC);
             <h4 class="mt-4">Pago</h4>
             <div class="form-group">
                 <label for="tipo_pago">Tipo de Pago</label>
-                <select name="tipo_pago" id="tipo_pago" class="form-control" required>
+                <select name="tipo_pago" id="tipo_pago" class="form-control" required onchange="toggleReferencia()">
                     <option value="">Seleccione un tipo de pago</option>
                     <option value="TRANSFERENCIA BANCARIA">TRANSFERENCIA BANCARIA</option>
                     <option value="PAGO MOVIL">PAGO MOVIL</option>
@@ -212,7 +212,7 @@ $result_agenda = $stmt_agenda->fetchAll(PDO::FETCH_ASSOC);
                 <label for="monto">Monto</label>
                 <input type="number" name="monto" id="monto" class="form-control" value="35" readonly required>
             </div>
-            <div class="form-group">
+            <div class="form-group" id="referencia-group">
                 <label for="referencia_bancaria">Referencia Bancaria</label>
                 <input type="text" name="referencia_bancaria" id="referencia_bancaria" class="form-control" required>
             </div>
@@ -226,10 +226,9 @@ $result_agenda = $stmt_agenda->fetchAll(PDO::FETCH_ASSOC);
     <script src="script/menorEdad2.js "></script>
     <script src="script/discapacitado.js"></script>
     <script src="script/relacion_discapacitado.js"></script>
+<script src="script/referencia.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
    
-
-
 </body>
 
 </html>
