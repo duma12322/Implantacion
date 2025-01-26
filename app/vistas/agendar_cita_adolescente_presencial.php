a8db4f3cec3ca0016faef3ee9ab6120017a94efd
Marcelis Dun <149423823+duma12322@users.noreply.github.com>
2025-01-25 21:32:08 -0400

    Update agendar_cita_adolescente_presencial.php

--- app/vistas/agendar_cita_adolescente_presencial.php
+++ app/vistas/agendar_cita_adolescente_presencial.php
@@ -200,10 +200,10 @@ $info_usuario = $stmt_usuario->fetch(PDO::FETCH_ASSOC);
                 <textarea name="motivo" id="motivo" class="form-control" rows="3" required></textarea>
             </div>
 
-            <h4 class="mt-4">Pago</h4>
+           <h4 class="mt-4">Pago</h4>
             <div class="form-group">
                 <label for="tipo_pago">Tipo de Pago</label>
-                <select name="tipo_pago" id="tipo_pago" class="form-control" required>
+                <select name="tipo_pago" id="tipo_pago" class="form-control" required onchange="toggleReferencia()">
                     <option value="">Seleccione un tipo de pago</option>
                     <option value="TRANSFERENCIA BANCARIA">TRANSFERENCIA BANCARIA</option>
                     <option value="PAGO MOVIL">PAGO MOVIL</option>
@@ -214,7 +214,7 @@ $info_usuario = $stmt_usuario->fetch(PDO::FETCH_ASSOC);
                 <label for="monto">Monto</label>
                 <input type="number" name="monto" id="monto" class="form-control" value="35" readonly required>
             </div>
-            <div class="form-group">
+            <div class="form-group" id="referencia-group">
                 <label for="referencia_bancaria">Referencia Bancaria</label>
                 <input type="text" name="referencia_bancaria" id="referencia_bancaria" class="form-control" required>
             </div>
@@ -228,7 +228,7 @@ $info_usuario = $stmt_usuario->fetch(PDO::FETCH_ASSOC);
     <script src="script/menorEdad2.js "></script>
     <script src="script/discapacitado.js"></script>
     <script src="script/relacion_discapacitado.js"></script>
-
+    <script src="script/referencia.js"></script>
     
 </body>
 
